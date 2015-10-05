2a170454260a392b7e10b9e020fc2b8b064513f5
Jack Somervell <jacksomervell@gmail.com>
2015-10-05 16:03:43 +0100

    added facebook api authentication and login button to the index page

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : 1527817464176113, // your App ID
+        'clientSecret'  : 'f09ce770571f13b66cda37df8e23ecc0', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    }
+  }
