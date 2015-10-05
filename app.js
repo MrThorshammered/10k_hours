2cd301dc194c330fda48d02595529b66bd7a6fcc
Jack Somervell <jacksomervell@gmail.com>
2015-10-05 16:29:19 +0100

    you can now register with facebook

--- app.js
+++ app.js
@@ -44,44 +44,44 @@ var User = require('./models/user')
 require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 //adding in seed data
-var firstLog = new Log({
-  name: 'My first swinging session',
-  date: '01/04/2001',
-  type: 'Practice',
-  hours: 2,
-  notes: 'It was hard',
-  video: 'www.myvideo.com'
-})
+// var firstLog = new Log({
+//   name: 'My first swinging session',
+//   date: '01/04/2001',
+//   type: 'Practice',
+//   hours: 2,
+//   notes: 'It was hard',
+//   video: 'www.myvideo.com'
+// })
 
-firstLog.save(function(err, log){
-  if (err) console.log(err)
-    console.log("first log saved")
-})
+// firstLog.save(function(err, log){
+//   if (err) console.log(err)
+//     console.log("first log saved")
+// })
 
-var Badge1 = new Badge({
-  name: "100 hours logged!",
-  image: "www.penis.com"
-})
+// var Badge1 = new Badge({
+//   name: "100 hours logged!",
+//   image: "www.penis.com"
+// })
 
-Badge1.save(function(err, badge){
-  if (err) console.log(err)
-    console.log("badge saved!")
-})
-var Jack = new User({
-  local: {
-    name: 'Jack Somervell',
-    email: 'jack@jack.com',
-    password: 'password',
-    discipline: 'Golf',
-    badges: Badge1,
-    logs: firstLog
-  }
-})
+// Badge1.save(function(err, badge){
+//   if (err) console.log(err)
+//     console.log("badge saved!")
+// })
+// var Jack = new User({
+//   local: {
+//     name: 'Jack Somervell',
+//     email: 'jack@jack.com',
+//     password: 'password',
+//     discipline: 'Golf',
+//     badges: Badge1,
+//     logs: firstLog
+//   }
+// })
 
-Jack.save(function(err, user){
-  if (err) console.log(err)
-    console.log("Jack saved")
-})
+// Jack.save(function(err, user){
+//   if (err) console.log(err)
+//     console.log("Jack saved")
+// })
 
 // launch ======================================================================
 app.listen(port);
